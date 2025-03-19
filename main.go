01cdde19f34e6eb10f2442216950d9fa5cd0ebf6
kiquetal <kiquetal@gmail.com>
2025-03-19 10:31:08 -0300

    using gochannel to read operations

--- main.go
+++ main.go
@@ -9,46 +9,80 @@ import (
 
 func main() {
 
-	var b = make([]byte, 8)
-	// Open file
 	file, err := os.Open("messages.txt")
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println("Error opening file:", err)
 		return
 	}
+	// Ensure the file is closed after reading
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Error closing file:", err)
+		}
+	}(file)
 
-	// Read file
-	n, err := file.Read(b)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
+	// Use the file as an io.ReadCloser
+	readCloser := io.ReadCloser(file)
+	// Open file
+	lines := getLinesChannel(readCloser)
+
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
-	currentLine := ""
-	// read while not EOF
-	for n > 0 {
 
-		parts := strings.Split(string(b), "\n")
+}
 
-		if len(parts) > 1 {
+func getLinesChannel(f io.ReadCloser) <-chan string {
 
-			currentLine += parts[0]
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = parts[1]
+	// Create channel
+	lines := make(chan string)
 
-		} else {
-			currentLine += parts[0]
-		}
+	go func() {
+		defer close(lines)
 
-		n, err = file.Read(b)
-		if err == io.EOF {
-			// Close file
-			file.Close()
-			return
-		}
+		var b = make([]byte, 8)
+		// Read file
+
+		n, err := f.Read(b)
 		if err != nil {
 			fmt.Println("Error: ", err)
 			return
 		}
-	}
+		currentLine := ""
+		// read while not EOF
+		for n > 0 {
 
+			parts := strings.Split(string(b), "\n")
+
+			if len(parts) > 1 {
+
+				currentLine += parts[0]
+
+				// Send line to channel
+
+				lines <- currentLine
+
+				currentLine = parts[1]
+
+			} else {
+				currentLine += parts[0]
+			}
+
+			n, err = f.Read(b)
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				fmt.Println("Error: ", err)
+				return
+			}
+
+		}
+		if err := f.Close(); err != nil {
+			fmt.Println("Error closing file:", err)
+		}
+	}()
+
+	return lines
 }
