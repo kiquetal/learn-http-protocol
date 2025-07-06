69cd00f0ec2dfede19320567e5c4ec89128c60c8
kiquetal <kiquetal@gmail.com>
2025-07-06 12:41:42 -0300

    Enhance RequestFromReader to read data in chunks and improve error handling

--- internal/request/request.go
+++ internal/request/request.go
@@ -55,9 +55,21 @@ func RequestFromReader_Latest(reader io.Reader) (*Request, error) {
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
 	r := &Request{State: intialized}
+	buffer := make([]byte, 8)
+
 	for {
 		//read by chunk, follow the num of read and num of parsed bytes
 		//do not use readLine
+		b, e := reader.Read(buffer)
+		if e != nil && e != io.EOF {
+			return nil, e
+		}
+		if b == 0 {
+			if r.State == done {
+				return r, nil // Return the request if it's done
+			}
+			continue // No data read, continue to next iteration
+		}
 
 	}
 }
@@ -87,10 +99,11 @@ func parseRequestLine(line string) (int, error) {
 
 	endOfLine := strings.Index(line, "\r\n")
 	if endOfLine == -1 {
-		return 0, 0
+		return 0, nil // No end of line found, return nil error
 	}
 	parts := strings.Split(line[:endOfLine], " ")
-	//THIS CAN NOT BE AN ERROR, BECAUS I'M READING BY CHUNK
-	//ASSUME I WONT HAVE THE FULL LINE
-
+	if len(parts) < 3 {
+		return 0, io.ErrUnexpectedEOF // Not enough parts for a valid request line
+	}
+	return endOfLine + len("\r\n"), nil
 }
