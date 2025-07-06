7ff8eeb350ca959e778893479d48a347f1a1bc6a
kiquetal <kiquetal@gmail.com>
2025-07-06 11:34:17 -0300

    Implement RequestFromReader and readLine functions to parse HTTP requests from io.Reader

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,9 @@
 package request
 
-import "io"
+import (
+	"io"
+	"strings"
+)
 
 type Request struct {
 	RequestLine RequestLine
@@ -14,4 +17,40 @@ type RequestLine struct {
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
 
+	firstLine, err := readLine(reader)
+	if err != nil {
+		return nil, err
+	}
+	parts := strings.Split(firstLine, " ")
+	if len(parts) < 3 {
+		return nil, io.ErrUnexpectedEOF // Not enough parts for a valid request line
+	}
+	return &Request{
+		RequestLine: RequestLine{
+			Method:        parts[0],
+			RequestTarget: parts[1],
+			HttpVersion:   parts[2],
+		}}, nil
+
+}
+
+func readLine(reader io.Reader) (string, error) {
+	buf := make([]byte, 0, 1024)
+	for {
+		var b [1]byte
+		n, err := reader.Read(b[:])
+		if n == 0 || err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", err
+		}
+		if b[0] == '\n' {
+			break
+		}
+		if b[0] != '\r' { // Ignore carriage return
+			buf = append(buf, b[0])
+		}
+	}
+	return string(buf), nil
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,10 +1,19 @@
 package request
 
 import (
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	require.Equal(t, "GET", r.RequestLine.Method)
+	require.Equal(t, "/", r.RequestLine.RequestTarget)
+	require.Equal(t, "HTTP/1.1", r.RequestLine.HttpVersion)
+
 }
