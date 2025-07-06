d4b2affb43c4fcba83427719cf61de2e6fd519a8
kiquetal <kiquetal@gmail.com>
2025-07-06 13:48:42 -0300

    Add NewHeader function and Parse method to Header type

--- internal/headers/header.go
+++ internal/headers/header.go
@@ -1,3 +1,11 @@
 package headers
 
 type Header map[string]string
+
+func NewHeader() Header {
+	return make(Header)
+}
+
+func (h Header) Parse(data []byte) (n int, done bool, err error) {
+	return 0, false, nil
+}
