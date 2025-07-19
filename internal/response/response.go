e2ed78ce9b5ae0c94328fa538efe8bcad5f3ca22
kiquetal <kiquetal@gmail.com>
2025-07-19 10:59:33 -0300

    Add status codes for HTTP response handling

--- internal/response/response.go
+++ internal/response/response.go
@@ -1,1 +1,20 @@
 package response
+
+type StatusCode int
+
+const (
+	StatusOK                  StatusCode = 200
+	StatusCreated             StatusCode = 201
+	StatusAccepted            StatusCode = 202
+	StatusNoContent           StatusCode = 204
+	StatusBadRequest          StatusCode = 400
+	StatusUnauthorized        StatusCode = 401
+	StatusForbidden           StatusCode = 403
+	StatusNotFound            StatusCode = 404
+	StatusMethodNotAllowed    StatusCode = 405
+	StatusInternalServerError StatusCode = 500
+	StatusNotImplemented      StatusCode = 501
+	StatusBadGateway          StatusCode = 502
+	StatusServiceUnavailable  StatusCode = 503
+	StatusGatewayTimeout      StatusCode = 504
+)
